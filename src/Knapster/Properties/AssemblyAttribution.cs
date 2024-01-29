80dfa7aee0c30d12c74d22b235c35f046dce2d6b
ApacheTech Solutions <p_matthews@live.co.uk>
2024-01-29 23:46:58 +0000

    fix: ABI update for 1.19.3

--- src/Knapster/Properties/AssemblyAttribution.cs
+++ src/Knapster/Properties/AssemblyAttribution.cs
@@ -1,13 +1,13 @@
 ﻿// ReSharper disable StringLiteralTypo
 
-[assembly: ModDependency("game", "1.18.15")]
+[assembly: ModDependency("game", "1.19.3")]
 
 [assembly:ModInfo(
     "Knapster",
     "knapster",
     Description = "Easier crafting and gathering, for those with low manual dexterity.",
     Side = "Universal",
-    Version = "2.6.3",
+    Version = "2.7.0",
     NetworkVersion = "1.0.0",
     Website = "https://apachegaming.net",
     Contributors = new[] { "Apache" },

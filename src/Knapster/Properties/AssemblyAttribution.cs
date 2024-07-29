0dfa72556e57683b604933e5dc7702da9e9078d4
ApacheTech Solutions <p_matthews@live.co.uk>
2024-07-29 01:13:22 +0100

    feat: convert to easyx mod template

--- src/Knapster/Properties/AssemblyAttribution.cs
+++ src/Knapster/Properties/AssemblyAttribution.cs
@@ -1,13 +1,13 @@
 ﻿// ReSharper disable StringLiteralTypo
 
-[assembly: ModDependency("game", "1.19.3")]
+[assembly: ModDependency("game", "1.19.8")]
 
 [assembly:ModInfo(
     "Knapster",
     "knapster",
     Description = "Easier crafting and gathering, for those with low manual dexterity.",
     Side = "Universal",
-    Version = "2.9.5",
+    Version = "2.10.0",
     NetworkVersion = "1.0.0",
     Website = "https://apachegaming.net",
     Contributors = ["Apache"],

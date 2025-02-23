80d3a9ffa6da060567c72a5f6948729299fb78ad
ApacheTech Solutions <p_matthews@live.co.uk>
2025-02-23 01:43:10 +0000

    feat: update version number

--- src/Knapster/Properties/AssemblyAttribution.cs
+++ src/Knapster/Properties/AssemblyAttribution.cs
@@ -7,7 +7,7 @@
     "knapster",
     Description = "Easier crafting and gathering, for those with low manual dexterity.",
     Side = "Universal",
-    Version = "2.13.0",
+    Version = "2.14.0",
     NetworkVersion = "1.0.0",
     Website = "https://apachegaming.net",
     Contributors = ["Apache"],

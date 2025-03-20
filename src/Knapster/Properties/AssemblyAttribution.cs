cf16b773cca53d247a278a3605a95006c7e657a9
ApacheTech Solutions <p_matthews@live.co.uk>
2025-03-20 19:54:56 +0000

    fix: loggers fail to initialise when starting game in 1.20.6 #34

--- src/Knapster/Properties/AssemblyAttribution.cs
+++ src/Knapster/Properties/AssemblyAttribution.cs
@@ -1,13 +1,13 @@
 ﻿// ReSharper disable StringLiteralTypo
 
-[assembly: ModDependency("game", "1.20.1")]
+[assembly: ModDependency("game", "1.20.6")]
 
 [assembly:ModInfo(
     "Knapster",
     "knapster",
     Description = "Easier crafting and gathering, for those with low manual dexterity.",
     Side = "Universal",
-    Version = "2.14.1",
+    Version = "2.14.2",
     NetworkVersion = "1.0.0",
     Website = "https://apachegaming.net",
     Contributors = ["Apache"],

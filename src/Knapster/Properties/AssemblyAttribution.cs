e67e7436116e90370fffb95b459a150d8560c841
ApacheTech Solutions <p_matthews@live.co.uk>
2025-06-18 00:47:31 +0100

    feat: update project files

--- src/Knapster/Properties/AssemblyAttribution.cs
+++ src/Knapster/Properties/AssemblyAttribution.cs
@@ -1,13 +1,10 @@
-﻿// ReSharper disable StringLiteralTypo
-
-[assembly: ModDependency("game", "1.20.6")]
-
+﻿[assembly: ModDependency("game", "1.20.11")]
 [assembly:ModInfo(
     "Knapster",
     "knapster",
     Description = "Easier crafting and gathering, for those with low manual dexterity.",
     Side = "Universal",
-    Version = "2.14.4",
+    Version = "2.14.6",
     NetworkVersion = "1.0.0",
     Website = "https://apachegaming.net",
     Contributors = ["Apache"],
